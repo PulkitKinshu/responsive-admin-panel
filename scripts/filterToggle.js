5b9e79c7bebf7896b16b676646da1b13e7140598
hp <pulkit@notationtech.com>
2020-04-25 20:21:43 +0530

    assignment complete

--- scripts/filterToggle.js
+++ scripts/filterToggle.js
@@ -1,9 +1,48 @@
+
+
+// For Navtabs
+
+$(function () {
+    $('a[data-toggle="tab"]').on('click', function (e) {
+        window.localStorage.setItem('activeTab', $(e.target).attr('href'));
+    });
+    var activeTab = window.localStorage.getItem('activeTab');
+    if (activeTab) {
+        $('#myTab a[href="' + activeTab + '"]').tab('show');
+        window.localStorage.removeItem("activeTab");
+    }
+});
+
+
+
+
+// For toggling Filters
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "350px";
-    document.getElementById("main").style.marginRight = "350px";
+    document.getElementById("mySidebar").style.marginRight = "0px";
+
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("mySidebar").style.marginRight = "-350px";
+
+}
+
+
+
+
+// For toggling Menu
+
+function openSideNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("mySideNav").style.marginLeft = "0px";
+
+}
+
+function closeSideNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("mySideNav").style.marginLeft = "-250px";
+
 }
